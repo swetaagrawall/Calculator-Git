848312723550a6d8336d5dede7b782e341e5fc82
sweta <swetaarawal972@gmail.com>
2025-01-07 12:06:59 +0530

    adding substraction file

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,15 @@ public class Calculator{
 	public static void main(String[] args){
            	System.out.println("Welcome to calculator");
 			System.out.println(addition(2, 5));
+			System.out.println(substraction(2, 5));
+
 
 	}
 	public static double addition(double num1, double num2) {
 		return num1 + num2;
+	}
+
+	public static double substraction(double num1, double num2) {
+		return num1 - num2;
 	}
 }
