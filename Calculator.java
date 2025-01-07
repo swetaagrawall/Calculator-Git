0824a3c1535387649e4aa80181a52a73de19f3fc
sweta <swetaarawal972@gmail.com>
2025-01-07 11:02:38 +0530

    Adding addition file

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator{
 	public static void main(String[] args){
            	System.out.println("Welcome to calculator");
+			System.out.println(addition(2, 5));
+
+	}
+	public static double addition(double num1, double num2) {
+		return num1 + num2;
 	}
 }
